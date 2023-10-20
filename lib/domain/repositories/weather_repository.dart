46af0a115a016b77cbce41e1d5d6afb6c4ec7eb4
Karol Ponikowski <k.ponikowski.98@gmail.com>
2023-10-20 14:41:44 +0200

    Add thunder and dio to project

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
   Future<WeatherModel?> getWeatherModel({
     required String city,
   }) async {
+    //http://api.weatherapi.com/v1/current.json?key=11c966fb78f14b7e96f123248232010&q=London&aqi=no
+    final response = await Dio().get(
+        'http://api.weatherapi.com/v1/current.json?key=11c966fb78f14b7e96f123248232010&q=London&aqi=no');
     return const WeatherModel(city: 'Warsaw', temperature: -5.5);
   }
 }
